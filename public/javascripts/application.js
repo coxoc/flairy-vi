3e8258bcf7416bc6945a7e18abfd803d49c82ae3
coxoc <coxoc_coder@gmail.com>
2010-05-04 16:58:30 +0900

    Add status text.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,9 +4,10 @@ Flairy.require([
 ])(function() {
   FlairyVi.Application = Ext.extend(Flame.Application,{
 
+    title: "flairy-vi",
     constructor: function(cfg) {
       cfg = cfg || {};
-      Ext.apply(cfg, {meta: {title: "vi"}});
+      Ext.apply(cfg, {meta: {title: this.title}});
       FlairyVi.Application.superclass.constructor.call(this, cfg);
     }
 
